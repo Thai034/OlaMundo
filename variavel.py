77896c04182ab6e513f1140fb397c99c65d8fe6f
Thaila Alves <thailasalvees@gmail.com>
2023-07-31 11:13:21 -0300

    criando o arquivo variavel.py

---
+++ variavel.py
@@ -0,0 +1,11 @@
+#input é uma função que permite perguntar algo ao usuário, comentario de uma linha
+nome = input("Qual é o seu nome?")
+
+'''
+Bloco
+de
+Comentário
+Permite que várias linhas se tornem um comentario
+'''
+
+print("Olá,",nome,". Tudo bem com você?")
