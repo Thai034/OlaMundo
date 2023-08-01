c4970409d0e7c970f52225837577deccaa39d47e
Thaila Alves <thailasalvees@gmail.com>
2023-08-01 10:00:14 -0300

    Resolvendo exercícios

---
+++ exercicio01.py
@@ -0,0 +1,16 @@
+#Etapa 1: Entrada de dados(o que eu vou perguntar para o usuário)
+print("Me informe 3 notas escolares suas: ")
+nota1 = float(input("informe sua nota: "))
+nota2 = float(input("informe sua nota: "))
+nota3 = float(input("informe sua nota: "))
+
+# Etapa 2: processamento de dados
+media = (nota1 + nota2 + nota3)/3
+
+#Etapa 3: saída de dados, (resposta na tela do usuário)
+print("Sua média é: ",media)
+
+
+
+
+
---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+num = int(input("informe um número: "))
+
+ant = num - 1
+suc = num + 1
+
+print("O antercessor é:", ant)
+print("O sucessor é:", suc)
